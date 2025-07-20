186593ef9a51e58fc0aa66222df89d72ed7b00ae
babyitachi <37737991+babyitachi@users.noreply.github.com>
2025-07-20 15:58:26 +0000

    Deploying to gh-pages from @ babyitachi/abhishrut-website@0d23c4221693c6c4c072059741d06cdba5bd3a89 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,14 +18,14 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/abhishrut-website/projects/";
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/abhishrut-website/repositories/";
@@ -59,51 +59,16 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "projects-project-1",
-          title: 'project 1',
+      },{id: "projects-intentrec",
+          title: 'IntentRec',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/abhishrut-website/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/abhishrut-website/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/abhishrut-website/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/abhishrut-website/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/abhishrut-website/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/abhishrut-website/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
+              window.location.href = "/abhishrut-website/projects/IntentRec/";
+            },},{id: "projects-reckross",
+          title: 'RecKross',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/abhishrut-website/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/abhishrut-website/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/abhishrut-website/projects/9_project/";
+              window.location.href = "/abhishrut-website/projects/RecKross/";
             },},{
         id: 'social-email',
         title: 'email',
